17e3bb284c46ea82c20965c53f0fe14c48a30cb6
Herin Soni <herinsoni3737@gmail.com>
2026-03-28 22:34:43 +0530

    disk changes

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const Database = require('better-sqlite3');
 const { dbPath } = require('./config');
+
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 
 const db = new Database(dbPath);
 
